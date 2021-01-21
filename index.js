92f400d1052dcee2d15e52592602ce9f30a11155
tiago <tiagossansao@gmail.com>
2021-01-21 17:33:51 -0300

    Finished nav responsivity

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const pt = document.querySelectorAll('span[lang=pt]');
 const en = document.querySelectorAll('span[lang=en]');
 const dropdownContent = document.querySelector('.dropdown-content');
+let width = window.innerWidth;
 
 function setLang(lang) {
   const langs = [en, pt];
@@ -15,9 +16,11 @@ function setLang(lang) {
   );
   langsObj[lang].forEach((element) => (element.style.display = 'block'));
   localStorage.setItem('language', lang);
+  language = lang;
+  setResponsive(width);
 }
 
-const language = localStorage.getItem('language');
+let language = localStorage.getItem('language');
 language === null ? setLang('en') : setLang(language);
 
 document.querySelector('body').addEventListener('close', function () {
@@ -47,3 +50,23 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./service-worker.js');
   });
 };
+
+/* JS "CSS" gambiarra kkj */
+
+function setResponsive(actualWidth) {
+  const elementsPt = document.querySelectorAll('.responsive[lang=pt]');
+  const elementsEn = document.querySelectorAll('.responsive[lang=en]');
+  const langsObj = { en: elementsEn, pt: elementsPt };
+  if (actualWidth < 700) {
+    langsObj[language].forEach((el) => el.style.display = 'none');
+  } else {
+    langsObj[language].forEach((el) => el.style.display = 'inline');
+  }
+}
+
+setResponsive(width);
+window.addEventListener("resize", (e) => {
+  width = window.innerWidth;
+  setResponsive(width);
+  console.log('fon');
+})
